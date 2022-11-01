dd8391a90e73cdde50e5a1d4a3436b2f5912f53d
Frank Meza <frankmezapdx@gmail.com>
2022-10-31 17:33:05 -0700

    create wrapper functions, add comment

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"flag"
 
+	"github.com/frankmeza/roomchat/pkg/connections"
 	"github.com/frankmeza/roomchat/pkg/db"
 	"github.com/frankmeza/roomchat/pkg/errata"
 	"github.com/frankmeza/roomchat/pkg/users"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
 )
 
 const HOST_AND_PORT string = "127.0.0.1:9990"
 
+func makeDbMigrations(dbConn *gorm.DB) error {
+	return dbConn.AutoMigrate(
+		&connections.Connection{},
+		&connections.Message{},
+		&users.User{},
+	)
+}
+
+func addPkgActions(server *echo.Echo) {
+	users.AddUserActions(server)
+	connections.AddConnectionActions(server)
+}
+
+// basically fossilized at this point
 func main() {
 	dbConn, err := db.GetDbConnection()
 	if err != nil {
@@ -21,7 +37,7 @@ func main() {
 		}))
 	}
 
-	err = dbConn.AutoMigrate(&users.User{})
+	err = makeDbMigrations(dbConn)
 	if err != nil {
 		panic(errata.CreateError(errata.ErrataParams{
 			Err:    err,
@@ -29,7 +45,7 @@ func main() {
 		}))
 	}
 
-	envMessage := "append -dev 1 to start locally on" + HOST_AND_PORT
+	envMessage := "append -dev 1 to start locally " + HOST_AND_PORT
 	envFlag := flag.Int("dev", 0, envMessage)
 
 	flag.Parse()
@@ -42,7 +58,6 @@ func main() {
 		server.Use(middleware.Recover())
 	}
 
-	users.AddUserActions(server)
-
+	addPkgActions(server)
 	server.Logger.Fatal(server.Start(HOST_AND_PORT))
 }
