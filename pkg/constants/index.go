8ed2685748e72a201f8cc4e961981555688d4def
Frank Meza <frankmezapdx@gmail.com>
2022-11-07 00:17:47 -0800

    just mix all of it

--- pkg/constants/index.go
+++ pkg/constants/index.go
@@ -1,24 +1,22 @@
 package constants
 
 const (
+	BIO         string = "bio"
 	CONNECTIONS string = "connections"
+	CURRENT_PIC string = "current_pic"
 	DB_ERROR    string = "dbConn is very virus"
 	DB_URL      string = "DB_URL"
 	EMAIL       string = "email"
 	ID          string = "id"
 	JWT_SECRET  string = "JWT_SECRET"
+	LOCATION    string = "location"
+	LOGIN_ERROR string = "username or password is not correct"
+	NAME        string = "name"
 	NO_PASSWORD string = "NO_PASSWORD"
 	PASSWORD    string = "password"
+	PICS        string = "pics"
 	SIGNING_KEY string = "SIGNING_KEY"
 	USER_PROPS  string = "user_props"
 	USERNAME    string = "username"
 	UUID        string = "uuid"
-
-	BIO         string = "bio"
-	CURRENT_PIC string = "current_pic"
-	LOCATION    string = "location"
-	NAME        string = "name"
-	PICS        string = "pics"
-
-	LOGIN_ERROR string = "username or password is not correct"
 )
